176f45fb275db594a9672eb3c11ea8cfd2c853fe
Anupama-n <anupaneupane1994@gmail.com>
2024-08-27 20:00:50 +0545

    initial commit

---
+++ src/components/Inputs.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { UilSearch,UilLocationPoint } from '@iconscout/react-unicons'
+
+function Inputs() {
+  return (
+    <div className="flex felx-row justify-center my-6">
+      <div className="flex flex-row w-3/4 item-center justify-center space-x-4">
+        <input type="text" className="text=x1 font-light p-2 w-full shadow-xl capitalize"></input>
+      </div>
+    </div>
+
+  )
+}
+
+export default Inputs
